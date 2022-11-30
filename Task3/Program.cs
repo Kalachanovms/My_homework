921f1d7fe2443468ce37e42ef354b4589e1b6000
Kalachanovms@gmail.com <>
2022-11-30 11:52:14 +0300

    Save_homework

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+void Tablesquare(string str)
+{
+    Console.WriteLine(str);
+    double number = int.Parse(Console.ReadLine());
+    double count = 1;
+    while ( count <= number)
+    {
+        double pow = Math.Pow(number, 3);
+        Console.Write(pow + " ");
+        count ++;
+    
+    }
+        Console.WriteLine();
+}
+
+Tablesquare("Write number N : ");
