81e59f1b0951955832f7f8f3eeb06337f8355683
Kalachanovms@gmail.com <>
2022-12-04 14:51:39 +0300

    one

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+
+int DataInput(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+
+}
