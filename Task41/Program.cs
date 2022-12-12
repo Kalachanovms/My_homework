2a214cbab16c9a6ae57880d9abd9468bceef61d0
Kalachanovms@gmail.com <>
2022-12-12 12:45:08 +0300

    Homework for seminar 6

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+System.Console.WriteLine("Введите количество чисел");
+
+int number = int.Parse(Console.ReadLine()); // задаем количество чисел 
+int count = 0;
+
+for (int i = 0; i < number; i++)
+{
+    Console.Write($"Введите число {i + 1} : "); // номер числа исходя из общего количества
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0) // сравнивание числа и считаем их 
+        count++;
+}
+
+System.Console.WriteLine($"Положительных чисел : { count }");
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double AddNumber(string str)
+{
+    System.Console.WriteLine(str);
+    double num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+Console.Clear();
+System.Console.WriteLine("Двe прямыe, заданныe уравнениями y = k1 * x + b1, y = k2 * x + b2");
+double numk1 = AddNumber("Введите k1");
+double numb1 = AddNumber("Введите b1");
+double numk2 = AddNumber("Введите k2");
+double numb2 = AddNumber("Введите b2");
+
+
+double x = (numb2 - numb1) / (numk1 - numk2);
+double y = numk1 * (numb2 - numb1) / (numk1 -numk2) +numb1;
+
+System.Console.WriteLine($"Точка перечесения двух прямых - [{x}, {y}]");
